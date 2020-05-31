638c0ae8ca9ff0fd0b24c175b50ff3df2322a451
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2020-05-31 16:54:23 -0600

    Update

--- Numerical_methods/Mathematical_tools/Orthogonality/ortho_test.C
+++ Numerical_methods/Mathematical_tools/Orthogonality/ortho_test.C
@@ -3,6 +3,11 @@
 // A simple program to test if two vectors are orthogonal to each other.
 // Assumes vectors are in a Euclidean three-space.
 
+# include "TAxis.h"
+# include "TCanvas.h"
+# include "TH1.h"
+# include "TLine.h"
+
 # include <iostream>
 
 using namespace std;
@@ -35,5 +40,31 @@ void ortho_test(  ) {
         cout << "Vectors are not orthogonal" << endl;
         cout << "Dot product = " << a_dot_b << endl;
     }
+
+    // Plotting vectors (just in the x-y plane)
+    TH1D * h0;
+    double x1 = 0;
+    double x2 = 0;
+    double y1 = 0;
+    double y2 = 0;
+    x1 = a[1] - 1.1*a[1];
+    x2 = b[1] + 1.1*b[1];
+
+    y1 = a[2] - 1.1*a[2];
+    y2 = b[2] + 1.1*b[2];
+
+    cout << "x1 = " << x1 << "\tx2 = " << x2 << endl;
+    cout << "y1 = " << y1 << "\ty2 = " << y2 << endl;
+    TCanvas * c0 = new TCanvas("c0", "c0", 600, 500);
+    c0->cd();
+    h0 = new TH1D("h0", "", 100, x1, x2);
+    h0->SetMinimum(y1);
+    h0->SetMaximum(y2);
+    h0->Draw("");
+    //TLine * l1 = new TLine(a[1], a[2], b[1], b[2]);
+    TLine * l1 = new TLine(a[1], a[2], b[1], b[2]);
+    l1->SetLineColor(kBlack);
+    l1->SetLineWidth(2);
+    l1->Draw("l");
 
 }
---
+++ Numerical_methods/Mathematical_tools/Orthogonality/ortho_test_C.d
@@ -0,0 +1,36 @@
+
+# DO NOT DELETE
+
+./ortho_test_C.so: /usr/include/root/TAxis.h /usr/include/root/TNamed.h
+./ortho_test_C.so: /usr/include/root/TObject.h /usr/include/root/Rtypes.h
+./ortho_test_C.so: /usr/include/root/RConfig.h /usr/include/root/RVersion.h
+./ortho_test_C.so: /usr/include/root/DllImport.h
+./ortho_test_C.so: /usr/include/root/Rtypeinfo.h /usr/include/root/snprintf.h
+./ortho_test_C.so: /usr/include/root/strlcpy.h
+./ortho_test_C.so: /usr/include/root/TGenericClassInfo.h
+./ortho_test_C.so: /usr/include/root/TSchemaHelper.h
+./ortho_test_C.so: /usr/include/root/TStorage.h
+./ortho_test_C.so: /usr/include/root/TVersionCheck.h
+./ortho_test_C.so: /usr/include/root/Riosfwd.h /usr/include/root/TBuffer.h
+./ortho_test_C.so: /usr/include/root/TString.h /usr/include/root/TMathBase.h
+./ortho_test_C.so: /usr/include/root/TAttAxis.h /usr/include/root/TArrayD.h
+./ortho_test_C.so: /usr/include/root/TArray.h /usr/include/root/TCanvas.h
+./ortho_test_C.so: /usr/include/root/TPad.h /usr/include/root/TVirtualPad.h
+./ortho_test_C.so: /usr/include/root/TAttPad.h /usr/include/root/TVirtualX.h
+./ortho_test_C.so: /usr/include/root/TAttLine.h /usr/include/root/TAttFill.h
+./ortho_test_C.so: /usr/include/root/TAttText.h
+./ortho_test_C.so: /usr/include/root/TAttMarker.h
+./ortho_test_C.so: /usr/include/root/GuiTypes.h /usr/include/root/Buttons.h
+./ortho_test_C.so: /usr/include/root/TQObject.h
+./ortho_test_C.so: /usr/include/root/TAttBBox2D.h /usr/include/root/TPoint.h
+./ortho_test_C.so: /usr/include/root/TAttCanvas.h
+./ortho_test_C.so: /usr/include/root/TCanvasImp.h /usr/include/root/TH1.h
+./ortho_test_C.so: /usr/include/root/TArrayC.h /usr/include/root/TArrayS.h
+./ortho_test_C.so: /usr/include/root/TArrayI.h /usr/include/root/TArrayF.h
+./ortho_test_C.so: /usr/include/root/Foption.h
+./ortho_test_C.so: /usr/include/root/TVectorFfwd.h
+./ortho_test_C.so: /usr/include/root/TVectorDfwd.h
+./ortho_test_C.so: /usr/include/root/TFitResultPtr.h
+./ortho_test_C.so: /usr/include/root/TLine.h
+./ortho_test_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+ortho_test_C__ROOTBUILDVERSION= 5.34/30
