1cf6beb69b824f0a161002555098480b5818af21
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2020-06-01 01:07:59 -0600

    Update interp.C

--- Numerical_methods/Mathematical_tools/interp.C
+++ Numerical_methods/Mathematical_tools/interp.C
@@ -10,5 +10,25 @@
 using namespace std;
 
 void interp() {
+
+    // Setup quadratic fit
+    Double_t x[3+1], y[3+1];
+    Double_t xmin, xmax;
+    cout << "Enter data points: " << endl;
+    Int_t i;
+    for (i = 1; i<=3; i++) {
+        x[i] = i; // get rid of after debugging
+        y[i] = i; // get rid of after debugging
+        // cout << "x[" << i << "] = ";
+        // cin >> x[i];
+        // cout << "y[" << i << "] = ";
+        // cin >> y[i];
+    }
+
+    xmin = -1; xmax = 10; // get rid of after debugging
+    // cout << "Minimum x: "; cin >> xmin;
+    // cout << "Maximum x: "; cin >> xmax;
     cout << "code has run succesfully" << endl;
 }
+
+Double_t intrpf( Double_t xi, Double_t x[], double y[] );
