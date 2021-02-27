0e4c1369ac640dbc1c71e2d8629926787f5a1b4b
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2021-02-26 20:01:27 -0700

    Added accounting code

---
+++ Accounting/sample_codes/account.C
@@ -0,0 +1,12 @@
+// account.C
+// A simple code for sorting accounts
+// Reads in data.txt
+
+# include "TMath.h"
+# include "TRandom3.h"
+# include <cmath>
+# include <cstdlib>
+# include <fstream>
+# include <iostream>
+
+using namespace std;
