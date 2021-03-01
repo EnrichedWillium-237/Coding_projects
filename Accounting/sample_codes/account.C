9064504388e34df7abd4e00cd496f24747cad72d
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2021-02-28 18:43:58 -0700

    Update account.C

--- Accounting/sample_codes/account.C
+++ Accounting/sample_codes/account.C
@@ -10,3 +10,22 @@
 # include <iostream>
 
 using namespace std;
+
+int main() {
+    int acctnum[1000];
+    char name[1000];
+    double balance[1000];
+
+    char input;
+    int search_acct;
+    int search_name;
+
+    cout << "Please enter the name of the file: \n";
+
+    char filename[50];
+    ifstream random;
+    cin.getline(filename, 50);
+    random.open(filename);
+
+    
+}
